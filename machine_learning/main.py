6682d14f72ff5468b815ba3dc6f2fcd0a2716fa9
sachin <sachinjaywal@gmail.com>
2024-09-03 19:12:41 -0400

    before making changes to distance calculation

--- machine_learning/main.py
+++ machine_learning/main.py
@@ -22,12 +22,28 @@ c = df.shape[1]         # Number of features in the data
 
 
 # Implementation of a custom k-means function
-def kmeans(X, k, max_iters=100):
-    # # Randomly initialize the centroids
-    centroids = X[np.random.choice(X.shape[0], k, replace=False)]
+def plot_graph(centroids, c_):
+    # Plot markers and colors
+    mark = ['D', 'o', '1', '>', '*', 'p', 's']  # List of markers
+    color = ['lime', 'blue', 'green', 'black', 'brown', 'pink']  # List of colors
+
+    # Plot the clusters
+    for i in range(k):
+        print(i)
+        cluster_points = X[c_ == i]
+        plt.scatter(cluster_points[:, 0], cluster_points[:, 1], marker=mark[i], s=50, c=color[i])
+
+    # Plot the centroids
+    plt.scatter(centroids[:, 0], centroids[:, 1], marker='+', c='red', s=400)
+    plt.show()
+
+
+def kmeans(X, k, max_iters=10):
+    # initialize the centroids using kmeans++
+    # centroids = X[np.random.choice(range(n), k, replace=False)]
     # print(f'centroids: {centroids}')
     # Choose centroids that are far apart
-    # centroids = np.array[(0,0), (100,0), (100,10), (160,0)]
+    centroids = [[0, 0], [100, 0], [100, 10], [160, 0]]
 
 
     for i in range(max_iters):
@@ -42,28 +58,31 @@ def kmeans(X, k, max_iters=100):
             closest_centroid = np.argmin(distances)
             cluster_assignments.append(closest_centroid)
 
-        c = np.array(cluster_assignments)
-
+        c_ = np.array(cluster_assignments)
 
         # Find the new centroids
-        centroids = np.array([X[c == k].mean(axis=0) for k in range(k)])
-    return centroids,c
+        centroids = np.array([X[c_ == k].mean(axis=0) for k in range(k)])
+        plot_graph(centroids, c_)
+
+    return centroids,c_
 
 
 # Run the k-means algorithm
 X = df.values
 centroids,c = kmeans(X, k)
 
+plot_graph(centroids, c)
+
 # Plot markers and colors
-mark = ['$S$', 'D', 'o', '1', '>', '*', 'p', 's']  # List of markers
-color = ['lime', 'blue', 'green', 'black', 'brown', 'pink']  # List of colors
-
-# Plot the clusters
-for i in range(k):
-    print(i)
-    cluster_points = X[c == i]
-    plt.scatter(cluster_points[:, 0], cluster_points[:, 1], marker=mark[i], s=50, c=color[i])
-
-# Plot the centroids
-plt.scatter(centroids[:, 0], centroids[:, 1], marker='+', c='red', s=400)
-plt.show()
+# mark = [ 'D', 'o', '1', '>', '*', 'p', 's']  # List of markers
+# color = ['lime', 'blue', 'green', 'black', 'brown', 'pink']  # List of colors
+#
+# # Plot the clusters
+# for i in range(k):
+#     print(i)
+#     cluster_points = X[c == i]
+#     plt.scatter(cluster_points[:, 0], cluster_points[:, 1], marker=mark[i], s=50, c=color[i])
+#
+# # Plot the centroids
+# plt.scatter(centroids[:, 0], centroids[:, 1], marker='+', c='red', s=400)
+# plt.show()
