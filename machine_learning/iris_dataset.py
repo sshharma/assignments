fd755e81fe1cb3e0f8f7814d49d2864204d849e9
sachin <sachinjaywal@gmail.com>
2024-09-16 19:47:37 -0400

    updated iris

--- machine_learning/iris_dataset.py
+++ machine_learning/iris_dataset.py
@@ -3,10 +3,13 @@ This is the implementation of the original k-means algorithm for Iris dataset.
 """
 
 import argparse
+from random import random
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score, adjusted_rand_score
 from sklearn.preprocessing import StandardScaler
 from machine_learning.customized_kmeans import plot_graph
 
@@ -24,18 +27,32 @@ def main():
     df = pd.read_csv(args.train_dir, header=None)  # (150, 4)
     # read 1-4 columns as features
     features = df.iloc[:, 0:4].values
-    original_labels = df.iloc[:, 4].values
     features = scaler.fit_transform(features)
 
+    ground_truth = df.iloc[:, 4].values                                         # read the 5th column as ground truth
+    iris_to_int = {'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2}           # map the flower to integer
+    ground_truth_int = np.array([iris_to_int[label] for label in ground_truth])       # convert the flower to integer
+
+
     # Running the original k-means algorithm
-    kmeans = KMeans(n_clusters=args.k, max_iter=args.max_iters)  # random_state=args.random_state)
+    kmeans = KMeans(n_clusters=args.k, max_iter=args.max_iters, init=random)  # random_state=args.random_state)
     kmeans.fit(features)
     centroids = kmeans.cluster_centers_
     labels = kmeans.labels_
     print(f'Final centroids: {centroids}')
 
+    # Calculate Stats
+    ari = adjusted_rand_score(ground_truth_int, labels)
+    inertia = kmeans.inertia_
+    silhouette = silhouette_score(features, labels)
     # plot the clusters
     plot_graph(centroids, labels, features)
+
+    # print the stats
+    print(f'Adjusted Rand Index: {ari}')
+    print(f'Inertia: {inertia}')
+    print(f'Silhouette Score: {silhouette}')
+
 
     # Plot the clusters using attributes 3 and 4
     plt.scatter(features[:, 2], features[:, 3], c=labels)
@@ -43,10 +60,6 @@ def main():
 
     plt.title('Plot using feature 3 and 4.')
     plt.show()
-
-    # Print accuracy
-    accuracy = np.mean(original_labels == labels)
-    print(f'Accuracy: {accuracy}')
 
 
 if __name__ == '__main__':
